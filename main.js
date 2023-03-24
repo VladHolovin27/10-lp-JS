7785f5f39c9bc3e70116d13c856c7b43cdf3ef58
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-03-24 17:54:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//1//
+function one(a, b, c) {
+    let array = [a, b, c];
+    return array[1];
+}
+console.log(array);
+
+//2//
+function two(name, age) {
+    const person = {
+        name: ['Bob', 'Smith'],
+        age: 32,
+}
+return person.name;
+}
+//3//
+function num(name) {
+    let n = name;
+    return n;
+  }
+  let re = num(Number(prompt("enter name")));
+  console.log(re);
